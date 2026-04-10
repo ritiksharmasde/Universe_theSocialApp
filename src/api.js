4440d500c60b36d7f162b4c673198938da93dd7a
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-10 23:39:08 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+  process.env.REACT_APP_API_BASE_URL || "https://universe-backend-xskf.onrender.com/api";
 
 export const SERVER_BASE_URL =
-  process.env.REACT_APP_SERVER_BASE_URL || "http://localhost:5000";
+  process.env.REACT_APP_SERVER_BASE_URL || "https://universe-backend-xskf.onrender.com";
 
 export default API_BASE_URL;
