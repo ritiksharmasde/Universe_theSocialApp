2517f02a218f6d35ff7111d8d1cb70680bba75b3
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-11 04:22:57 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const app = require("./app");
@@ -9,12 +8,17 @@ const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://joinuniverse.co.in",
+  "https://www.joinuniverse.co.in",
+  process.env.CLIENT_URL,
+].filter(Boolean);
+
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      process.env.CLIENT_URL,
-    ].filter(Boolean),
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
