858d04d982752923faa415f98ed5ff813ead32ce
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-11 00:16:45 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -27,7 +27,7 @@ app.use(
 
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
 app.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()");
