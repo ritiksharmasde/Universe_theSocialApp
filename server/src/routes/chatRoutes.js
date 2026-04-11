5a9687fcff63859d0d5a915943274809e6fbda7e
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-12 00:36:59 +0530

    Add delete conversation route

--- server/src/routes/chatRoutes.js
+++ server/src/routes/chatRoutes.js
@@ -4,11 +4,13 @@ const {
   getUserConversations,
   getConversationMessages,
   getOrCreateDirectConversation,
+  deleteConversationForMe,
 } = require("../controllers/chatController");
 
 const router = express.Router();
 
 router.post("/conversation", createOrGetConversation);
+router.post("/delete-for-me", deleteConversationForMe);
 router.post("/direct", getOrCreateDirectConversation);
 router.get("/conversations/:email", getUserConversations);
 router.get("/messages/:conversationId", getConversationMessages);
