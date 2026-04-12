692f9fa29d02d8f129663117c84385c2c339fd67
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-12 23:34:56 +0530

    Add resendOtp route to authRoutes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { sendOtp, verifyOtp , loginUser } = require("../controllers/authController");
+const { sendOtp, verifyOtp , loginUser, resendOtp } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
+router.post("/resend-otp", resendOtp);
 router.post("/login", loginUser);
 
 
