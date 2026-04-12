9384950003da6d02e857e5e0e424aa6369cda9d9
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-12 18:39:06 +0530

    Update MessagesPage.js

--- src/MessagesPage.js
+++ src/MessagesPage.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useMemo, useState, useRef } from "react";
 import { FiSend, FiSearch, FiMenu } from "react-icons/fi";
-import { io } from "socket.io-client";
+import socket from "./socket";
 import API_BASE_URL, {SERVER_BASE_URL} from "./api";
 import useBreakpoint from "./useBreakpoint";
-const socket = io(SERVER_BASE_URL);
 
 
 function MessagesPage({
