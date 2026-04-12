27c6a749422a13de3d76b0c75089c6204dbb4f88
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-12 21:25:57 +0530

    Update mailer.js

--- server/src/config/mailer.js
+++ server/src/config/mailer.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // use TLS
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  family: 4, // ✅ FORCE IPv4 (fixes your error)
 });
 
 module.exports = transporter;
