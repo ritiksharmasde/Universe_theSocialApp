aed8a883b550efa2b41d2628ed3ea095373df915
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-22 14:15:37 +0530

    Update postRoutes.js

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -6,13 +6,14 @@ const {
   getComments,
   addComment,
 } = require("../controllers/postController");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
 
-router.get("/", getPosts);
-router.post("/:postId/like", likePost);
-router.delete("/:postId/like", unlikePost);
-router.get("/:postId/comments", getComments);
-router.post("/:postId/comments", addComment);
+router.get("/", requireAuth, getPosts);
+router.post("/:postId/like", requireAuth, likePost);
+router.delete("/:postId/like", requireAuth, unlikePost);
+router.get("/:postId/comments", requireAuth, getComments);
+router.post("/:postId/comments", requireAuth, addComment);
 
 module.exports = router;
