30196b8de60bec5ea4a9e623c7235459d59e9327
RITIK SHARMA <RITIK.17886@stu.upes.ac.in>
2026-04-25 02:48:48 +0530

    Update AppLayout.js

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -30,11 +30,11 @@ function AppLayout({
   style={{
     ...styles.doodleOverlay,
     background:
-      theme === "dark"
-        ? "rgba(0, 0, 0, 0.45)"
-        : theme === "grey"
-        ? "rgba(0, 0, 0, 0.2)"
-        : "rgba(255, 255, 255, 0.4)",
+  theme === "dark"
+    ? "rgba(0, 0, 0, 0.28)"
+    : theme === "grey"
+    ? "rgba(0, 0, 0, 0.12)"
+    : "rgba(255, 255, 255, 0.55)",
   }}
 />
       <Sidebar
